b8af8a44d2b0769b7462e120d5b7f95d583cbf8a
Jan Horacek <me@apophis.cz>
2018-10-22 13:17:26 +0200

    Take string by reference.

--- q-str-exception.h
+++ q-str-exception.h
@@ -8,7 +8,7 @@ private:
 	QString m_err_msg;
 
 public:
-	QStrException(const QString msg) : m_err_msg(msg) {};
+	QStrException(const QString& msg) : m_err_msg(msg) {};
 	~QStrException() throw() {};
 	const QString str() const throw () { return this->m_err_msg; };
 	operator QString() const { return this->m_err_msg; }

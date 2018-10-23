23bb91e5059db1de809de1ef1c191cb403c0f1bb
Jan Horacek <me@apophis.cz>
2018-10-23 18:49:54 +0200

    Remove redundant semicolon.

--- q-str-exception.h
+++ q-str-exception.h
@@ -8,9 +8,9 @@ private:
 	QString m_err_msg;
 
 public:
-	QStrException(const QString& msg) : m_err_msg(msg) {};
-	~QStrException() throw() {};
-	const QString str() const throw () { return this->m_err_msg; };
+	QStrException(const QString& msg) : m_err_msg(msg) {}
+	~QStrException() throw() {}
+	const QString str() const throw () { return this->m_err_msg; }
 	operator QString() const { return this->m_err_msg; }
 };
 

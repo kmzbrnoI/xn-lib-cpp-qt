b0f718f715240cda2eceda94808b1547d6d4194a
Jan Horacek <me@apophis.cz>
2019-12-23 11:20:29 +0100

    Add dynamic-library API template.

---
+++ lib-api.cpp
@@ -0,0 +1,7 @@
+#include "lib-api.h"
+
+namespace Xn {
+
+XpressNet xn;
+
+} // namespace Xn
---
+++ lib-api.h
@@ -0,0 +1,12 @@
+#ifndef LIBAPI_H
+#define LIBAPI_H
+
+#include "xn.h"
+
+namespace Xn {
+
+extern XpressNet xn;
+
+} // namespace Xn
+
+#endif

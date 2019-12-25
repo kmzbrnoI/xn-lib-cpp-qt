b6fa35ace72c9aa6864704d7860fcf05f5e97764
Jan Horacek <me@apophis.cz>
2019-12-25 21:33:48 +0100

    Add rest of dynamic API functions.

--- lib-api.h
+++ lib-api.h
@@ -16,6 +16,16 @@ struct LibStdCallback {
 	void *const data;
 };
 
+struct LocoInfo {
+	uint16_t addr;
+	bool direction;
+	uint8_t speed;
+	uint8_t maxSpeed;
+	uint32_t functions;
+};
+
+using TrkAcquiredCallback = void(*)(const void *sender, LocoInfo);
+
 XN_SHARED_EXPORT bool CALL_CONV apiSupportsVersion(unsigned int version);
 XN_SHARED_EXPORT int CALL_CONV apiSetVersion(unsigned int version);
 XN_SHARED_EXPORT unsigned int CALL_CONV features();
@@ -27,6 +37,19 @@ XN_SHARED_EXPORT bool CALL_CONV connected();
 XN_SHARED_EXPORT int CALL_CONV trackStatus();
 XN_SHARED_EXPORT void CALL_CONV setTrackStatus(unsigned int trkStatus, LibStdCallback ok,
                                                LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV emergencyStop(LibStdCallback ok, LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV locoEmergencyStop(uint16_t addr, LibStdCallback ok,
+                                                  LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV locoSetSpeed(uint16_t addr, int speed, int dir, LibStdCallback ok,
+                                             LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV locoSetFunc(uint16_t addr, uint32_t funcMask, uint32_t funcState,
+                                            LibStdCallback ok, LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV locoAcquire(uint16_t addr, TrkAcquiredCallback,
+                                            LibStdCallback err);
+XN_SHARED_EXPORT void CALL_CONV locoRelease(uint16_t addr, LibStdCallback ok);
+
+XN_SHARED_EXPORT void CALL_CONV pomWriteCv(uint16_t addr, uint16_t cv, uint8_t value,
+                                           LibStdCallback ok, LibStdCallback err);
 
 XN_SHARED_EXPORT void CALL_CONV bindBeforeOpen(TrkStdNotifyEvent f, void *data);
 XN_SHARED_EXPORT void CALL_CONV bindAfterOpen(TrkStdNotifyEvent f, void *data);

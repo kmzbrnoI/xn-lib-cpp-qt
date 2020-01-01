39cfbf009bbe8d6115f75e5a1d9fd02e0d03f921
Jan Horacek <me@apophis.cz>
2020-01-01 16:18:18 +0100

    Fix loco address computation.

--- xn-loco-addr.h
+++ xn-loco-addr.h
@@ -24,7 +24,14 @@ struct LocoAddr {
 			throw EInvalidAddr("Invalid loco address!");
 	}
 
-	LocoAddr(uint8_t lo, uint8_t hi) : LocoAddr(lo + ((hi-0xC0) << 8)) {}
+	LocoAddr(uint8_t lo, uint8_t hi) {
+		if (hi == 0)
+			this->addr = lo;
+		else
+			this->addr = lo + ((hi-0xC0) << 8);
+		if (this->addr > 9999)
+			throw EInvalidAddr("Invalid loco address!");
+	}
 
 	uint8_t lo() const { return addr & 0xFF; }
 	uint8_t hi() const { return ((addr >> 8) & 0xFF) + (addr < 100 ? 0 : 0xC0); }

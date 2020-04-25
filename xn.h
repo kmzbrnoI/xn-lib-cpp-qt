6fff3538326c3f10610d22464cc1619cbad7deee
Jan Horacek <me@apophis.cz>
2020-04-25 13:22:18 +0200

    Trim data log.

--- xn.h
+++ xn.h
@@ -283,8 +283,7 @@ QString XpressNet::dataToStr(DataT data, size_t len) {
 	size_t i = 0;
 	for (auto d = data.begin(); (d != data.end() && (len == 0 || i < len)); d++, i++)
 		out += QString("0x%1 ").arg(static_cast<ItemType>(*d), 2, 16, QLatin1Char('0'));
-
-	return out;
+	return out.trimmed();
 }
 
 template <typename Target>
